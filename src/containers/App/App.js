57c26c0ce79664b9a6bbc2487df5f4985a7aa767
Daniel Goerdes <d.goerdes@emakina.at>
2018-01-15 17:15:49 +0100

    add some notes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Shell from '../../components/Shell/Shell';
 import Headline from '../../components/Headline/Headline';
 
 /**
@@ -9,14 +8,22 @@ import Headline from '../../components/Headline/Headline';
  */
 const App = () => {
     return (
-        <Shell>
+        <div>
             <Headline weight={1}>Headline H1</Headline>
             <Headline weight={2}>Headline H2</Headline>
             <Headline weight={3}>Headline H3</Headline>
             <Headline weight={4}>Headline H4</Headline>
             <Headline weight={5}>Headline H5</Headline>
             <Headline weight={6}>Headline H6</Headline>
-        </Shell>
+
+            <div>
+                <ul>
+                    <li>How to do reset/global styles?</li>
+                    <li>How to do element styles?</li>
+                    <li>Keyframes?!</li>
+                </ul>
+            </div>
+        </div>
     );
 };
 
